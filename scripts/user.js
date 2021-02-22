63cb4630fed2ae92b5eb77f296f6041256ee348a
Ruben <iruben229@gmail.com>
2021-02-22 17:43:56 -0500

    Creating the method that will help generate the html related to the user's profile

--- scripts/user.js
+++ scripts/user.js
@@ -7,6 +7,22 @@ export class User {
     }
 
     generateHTML() {
-        
+        return `
+            <div class="user">
+                <picture class="user__image">
+                    <source srcset="${this.profile.image}" media="(min-width: 320px)">
+                    <img src="${this.profile.image}" alt="profile picture">
+                </picture>
+
+                <div class="user__info">
+                    <span>${this.profile.name}</span>
+                    <span>${this.profile.position}</span>
+                </div>
+            </div>
+        `;
+    }
+
+    insertHTML(element, html) {
+
     }
 }
