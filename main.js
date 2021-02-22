18d76a70531215c55d633b8e17adbc0567ee7e99
Ruben <iruben229@gmail.com>
2021-02-22 17:43:06 -0500

    Move the createUsers function outside of the return object to maintain its privacy

--- main.js
+++ main.js
@@ -4,19 +4,23 @@ import { getUsers } from './scripts/index.js';
 const userModule = (() => {
     const USER_ENDPOINT = 'https://leaflink-engineering-interview-docs.s3-us-west-2.amazonaws.com/public/frontend/UserProfileCard/user-profile-data.json';
     
+    const createUsers = () => {
+        const profiles = getUsers(USER_ENDPOINT);
+        const element = document.querySelector('.user-gallery');
+
+        profiles.then(profiles => {
+            for (const profile of profiles) {
+                const user = new User(profile);
+                const component = user.generateHTML();
+                user.insertHTML(element, component);
+            }
+        })
+    };
+
     return {
-        createUsers: () => {
-            const profiles = getUsers(USER_ENDPOINT);
-            profiles.then(profiles => {
-                for (const profile of profiles) {
-                    const user = new User(profile);
-                    user.generateHTML();
-                }
-            })
-        },
-        initialize: function() {
-            this.createUsers();
-        }   
+        initialize: () => {
+            createUsers();
+        }
     };
 })();
 
