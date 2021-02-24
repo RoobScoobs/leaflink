f76e0b1f5b23cf8e95d17b08517b28fd4aa69924
Ruben <iruben229@gmail.com>
2021-02-24 18:53:38 -0500

    Include the click listener for the expandable card feature

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import { User } from './scripts/index.js';
-import { getUsers } from './scripts/index.js';
+import { User, getUsers, toggleUserDetails } from './scripts/index.js';
 
 const userModule = (() => {
     const USER_ENDPOINT = 'https://leaflink-engineering-interview-docs.s3-us-west-2.amazonaws.com/public/frontend/UserProfileCard/user-profile-data.json';
+    const element = document.querySelector('.user-gallery');
     
     const createUsers = () => {
         const profiles = getUsers(USER_ENDPOINT);
-        const element = document.querySelector('.user-gallery');
 
         profiles.then(profiles => {
             for (const profile of profiles) {
@@ -20,8 +19,12 @@ const userModule = (() => {
     return {
         initialize: () => {
             createUsers();
-        }
+        },
+        attachListeners: (fn, type) => {
+            element.addEventListener(type, fn);
+        },
     };
 })();
 
 userModule.initialize();
+userModule.attachListeners(toggleUserDetails, 'click');
